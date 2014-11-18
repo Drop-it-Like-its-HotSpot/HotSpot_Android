3e7aa3ab6d0e7a13ec3e499cb153c4cc5c2f9cc1
Nick Tardif <nicktardif@gmail.com>
2014-11-18 16:35:43 -0500

    Updated Message class

--- app/src/main/java/com/example/ticknardif/hotspot/Message.java
+++ app/src/main/java/com/example/ticknardif/hotspot/Message.java
@@ -1,25 +1,41 @@
 package com.example.ticknardif.hotspot;
 
+import java.util.Date;
+
 public class Message {
-    private String content;
-    private int senderID;
-    private String senderName;
+    private int m_id;
 
-    public Message(String content, int senderID, String senderName) {
-        this.content = content;
-        this.senderID = senderID;
-        this.senderName = senderName;
+    private int Room_id;
+    private int User_id;
+    private Date TimeStamp;
+
+    private String Message;
+
+    public Message(int m_id, int room_id, int user_id, Date timeStamp, String message) {
+        this.m_id = m_id;
+        Room_id = room_id;
+        User_id = user_id;
+        TimeStamp = timeStamp;
+        Message = message;
     }
 
-    public String getContent() {
-        return content;
+    public int getM_id() {
+        return m_id;
     }
 
-    public int getSenderID() {
-        return senderID;
+    public int getRoom_id() {
+        return Room_id;
     }
 
-    public String getSenderName() {
-        return senderName;
+    public int getUser_id() {
+        return User_id;
+    }
+
+    public Date getTimeStamp() {
+        return TimeStamp;
+    }
+
+    public String getMessage() {
+        return Message;
     }
 }
